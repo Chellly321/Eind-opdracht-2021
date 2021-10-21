c206d7f8ce6f7b2fa5c90d7cf717b1931925fb24
Chelly321 <michelle.royer@icloud.com>
2021-10-21 16:29:57 +0200

    persoon aanmaken

---
+++ src/main/java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    // Attributen
+    private String Name;
+    private String Lastname;
+    private String Emailaddress;
+    private int Age;
+    private int Phonenumber;
+
+
+
+
+}
